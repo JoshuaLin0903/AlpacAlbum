4c7574f20b686acd23833e7a46a93f18856af729
JoshuaLin0903 <b06901016@ntu.edu.tw>
2021-01-06 23:30:53 +0800

    upd imgur upload

--- src/components/All.js
+++ src/components/All.js
@@ -1,14 +1,52 @@
 import React, {useState} from 'react'
 import '../style.css'
-import {Breadcrumb, Button, Modal, Divider, Input, Tag, Upload} from 'antd';
+import {Breadcrumb, Button, Modal, Divider, Input, Tag, Upload, message} from 'antd';
 import {UploadOutlined, FolderAddOutlined, InboxOutlined} from '@ant-design/icons';
+import axios from 'axios';
 
 const { Dragger } = Upload;
+const client_id = 'bcdefbeb2fcc6da';
 
-export const ALL= () =>{
+export const ALL = () =>{
 	const [uploadvis, setUploadvis] = useState(false)
 	const [addvis, setAddvis] = useState(false)
 	const [state, setState] = useState('none')
+
+	const [urllist, setUrls] = useState([])
+
+	// imgur upload props
+	const props = {
+		name: 'file',
+		action: 'https://api.imgur.com/3/image',
+		headers: {
+			Authorization: `Client-ID ${client_id}`
+		},
+		method: 'POST',
+		onChange: (info)=>{
+			if (info.file.status !== 'uploading') {
+				console.log(info.file, info.fileList);
+			}
+			if (info.file.status === 'done') {
+				message.success(`${info.file.name} file uploaded successfully`);
+			}
+			else if (info.file.status === 'error') {
+				message.error(`${info.file.name} file upload failed.`);
+			}
+		},
+		customRequest: (info) => {
+			const data = new FormData();
+			data.append('image', info.file);
+			const config = { headers: info.headers };
+			axios.post(info.action, data, config).then((res) => {
+				const imgUrl = res.data.data.link
+				console.log(imgUrl)
+				setUrls([...urllist, imgUrl])
+				info.onSuccess(res.data, info.file)
+			}).catch((err) => {
+				info.onError(err, info.file)
+			})
+		}
+	};
 
 	return(
 		<>
@@ -38,7 +76,7 @@ export const ALL= () =>{
        				:
        			</div>
        			<Divider/>
-       			<Dragger>
+       				<Dragger {...props}>
     					<p className="ant-upload-drag-icon">
       					<InboxOutlined />
     					</p>
@@ -67,7 +105,13 @@ export const ALL= () =>{
    		</Modal>
 
 			<div className="main-display-all">
-				No photos ...
+				{urllist.length === 0 ? (
+					<p>No photos ...</p>
+				):(
+					urllist.map((url, i) => {
+						return (<p key={i}><a target="_blank" rel="noopener noreferrer" href={url}>image {i}</a></p>)
+					})
+				)}
 			</div>
 		</>
 	)
