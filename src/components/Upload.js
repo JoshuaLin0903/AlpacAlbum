4f8884ac3eaf93901ea31ff45438e1e487faf119
YYOOGGAA <b06901144@ntu.edu.tw>
2021-01-18 17:01:48 +0800

    change upload style

--- src/components/Upload.js
+++ src/components/Upload.js
@@ -1,8 +1,9 @@
 import React, {useState, useEffect, useRef} from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks'
-import {Breadcrumb, Input, Upload, message, Tag, Divider, Button, Popconfirm, AutoComplete} from 'antd'
-import {RocketTwoTone, InboxOutlined} from '@ant-design/icons'
+import {Breadcrumb, Input, Upload, message, Tag, Divider, Button, Popconfirm} from 'antd'
+import {RocketTwoTone, InboxOutlined, UploadOutlined, SearchOutlined, StarFilled} from '@ant-design/icons'
 import axios from 'axios';
+import 'antd/dist/antd.css';
 
 import '../style.css';
 import { IMAGE_CREATE } from '../graphql/images'
@@ -11,11 +12,13 @@ import {
   } from '../graphql/tags'
 
 const { Dragger } = Upload;
+const { CheckableTag } = Tag;
 const client_id = 'bcdefbeb2fcc6da';
 
 export const UPLOAD = ({ user, AppWhenUpload, updPics, setUpdPics}) =>{
-	const [tagValue, setTagValue] = useState('')
 	const [select, setSelect] = useState([])
+	const [showingTags, setShowingTags] = useState([])
+  const [searchValue, setSearchValue] = useState('')
 	const [open, setOpen] = useState(false)
 	const [fileList, setFileList] = useState([])
 	const [uploading, setUploading] = useState(false)
@@ -42,20 +45,39 @@ export const UPLOAD = ({ user, AppWhenUpload, updPics, setUpdPics}) =>{
 			return false
 		},
 		defaultFileList: fileList,
-		fileList,
+		fileList
 	};
 
 	const selectTag = () => {
 		var buf = select
-		if (buf.indexOf(tagValue) === -1 && tagValue !== ''){
-			buf.push(tagValue)
+		console.log(select)
+		console.log(searchValue)
+		if (select.indexOf(searchValue) === -1 && searchValue !== ''){
+			buf.push(searchValue)
+			if (tagData.tags.indexOf(searchValue) === -1){
+				const str = 'A new tag is added: "'+ searchValue + '".'
+				message.info({
+					content: str,
+					style: {
+						marginTop: '40vh',
+						marginRight: '30vw'
+					}
+				})
+			}
 			setSelect(buf)
 		}
-		else if (buf.indexOf(tagValue) !== -1){
-			const str = '"'+ tagValue + '" has already been selected.'
-			message.info(str)
+		else if (select.indexOf(searchValue) !== -1){
+			const str = '"'+ searchValue + '" has already been selected.'
+			message.info({
+				content: str,
+				style: {
+					marginTop: '40vh',
+					marginRight: '40vw'
+				}
+			})
 		}
-		setTagValue('')
+		setSearchValue('')
+		setShowingTags(tagData.tags)
 	}
 
 	const handleUpload = async() => {
@@ -124,15 +146,38 @@ export const UPLOAD = ({ user, AppWhenUpload, updPics, setUpdPics}) =>{
 		setSelect(buf)
 	}
 
-	const Options = tagLoading ? [] : tagData.tags.map((m) =>{
-		return {value: m}
-	})
-
 	useEffect(()=>{
 		isMountedRef.current = true
 		tagRefetch()
 		return () => isMountedRef.current = false
 	},[])
+
+	const filterTags = (search) => {
+    const l = search.length
+    if(l === 0){
+      setShowingTags(tagData.tags)
+    }
+    else{
+      const _filter = tagData.tags.filter((t) => 
+        (t.toLowerCase().substr(0,l) === search.toLowerCase())
+      )
+      setShowingTags(_filter)
+    }
+    
+  }
+
+  const chooseTags = (tag, checked) => {
+    const nextSelectedTags = checked ? [...select, tag] : select.filter(t => t !== tag);
+    setSelect(nextSelectedTags);
+  }
+
+  useEffect(()=> {
+    if(tagData){
+      if(tagData.tags){
+        setShowingTags(tagData.tags)
+      }
+    }
+  }, [tagData])
 
 	return(
 		<>
@@ -141,83 +186,79 @@ export const UPLOAD = ({ user, AppWhenUpload, updPics, setUpdPics}) =>{
 					<RocketTwoTone twoToneColor="#eb2f96" /> Upload new photos here!
 				</Breadcrumb.Item>
 			</Breadcrumb>
-			<div className="main-display-cen">
-				<div style={{width: 350, textAlign:"center"}}>
-					<h1 style={{fontSize: 35, marginBottom:25, color:"#00008B"}}> Upload to ... </h1>
-					<AutoComplete
-						style={{marginBottom: 10, width: 350}}
-						open={open}
-						onBlur={() => setOpen(false)}
-						onClick={() => setOpen(true)}
-						onSelect={()=> setOpen(false)}
-						value={tagValue}
-						options={Options}
-						filterOption={(inputValue, option) =>
-      				option.value.toUpperCase().substr(0,inputValue.length) === inputValue.toUpperCase()
-    				}
-    				onChange={(value) => {setTagValue(value);setOpen(true)}}
-					>
-       			<Input.Search
-       				enterButton="Enter"
-       		 		placeholder="Enter the tag you want"
-       		 		onChange={(e) => {setTagValue(e.target.value)}}
-       		 		onSearch={() => {selectTag();setOpen(false);}}
-       		 	/>
-       		 </AutoComplete>
-       		<div style={{textAlign:"left"}}>
-       			<Tag color="geekblue"> Selected </Tag>
-       			:
-       			{select.map((tag) => (
-         			<Tag 
-         				key={tag} 
-         				closable={true} 
-         				style={(select.indexOf(tag)===0)?
-         					{marginLeft: "5px"}:{marginLeft: "0px"}} 
+			<div className="main-display-up">
+				<h1 style={{fontSize: 35, marginBottom:25, color:"#00008B"}}> <UploadOutlined /> Upload to ... </h1>
+				<div className="upload-div">
+					<div style={{width: "50%"}}>
+						<div className="upload-tag">
+							<h3 style={{color: "white"}}> Search a tag or enter a new tag. </h3>
+							<Input allowClear value={searchValue} prefix={<SearchOutlined/>} style={{ width: "70%" }} 
+								onChange={(e) => {filterTags(e.target.value);setSearchValue(e.target.value);}}
+								onKeyDown={(e) => {
+                  if(e.key === 'Enter') {selectTag()}
+                }}/>
+		 					<div className="chosen_tag_block-up">
+        				<Tag icon={<StarFilled />} color="purple" style={{marginTop: "5px"}}>
+        					Selected
+        				</Tag>
+        				: 
+          			{select.map(tag => (
+         				<Tag key={tag} closable={true} color={(tagData.tags.indexOf(tag) === -1)?"#87d068":"#108ee9"}
+         				style={(select.indexOf(tag)===0)?{marginLeft: "5px",marginTop: "5px"}:{marginLeft: "0px",marginTop: "5px"}} 
              		onClose={() => closeTags(tag)}>
-             		{tag}
-            	</Tag>
-          	))}
+             			{tag}
+            		</Tag>
+          			))}
+    	  			</div>
+            	{tagLoading ? (<p>loading</p>):
+            		(<div className="tags_group-up">
+    	  					{(tagData.tags.length === 0) ?
+          					<p style={{color:"gray", textAlign: "center"}}> No tags ... </p>:
+         						((showingTags.length === 0)?
+          						<p style={{color:"gray", textAlign: "center"}}> No tags are found </p>
+          						:
+          						(<><h4 style={{color: "#3b5999"}}> Click the tag you want to add </h4>
+          						{showingTags.map(tag => {
+          							return(
+  												<CheckableTag	key={tag}	checked={select.indexOf(tag) > -1}
+   													onChange={checked => chooseTags(tag, checked)}>
+   				 									#{tag}
+  												</CheckableTag>)})
+ 											}	
+  										</>)
+  									)	
+ 									}
+ 								</div>)}
+       			</div>
        		</div>
-       		{select.length?
-          		<Button size="small" 
-          		style={{marginTop:15, borderColor:"#E9967A", fontSize: 12}} 
-          		danger
-          		onClick={() => setSelect([])}
-          		shape="round"> 
-          			Clear All 
-          		</Button>:
-          		<div style={{marginTop:15}}></div>
-          }
-       		<Divider/>
-       		<Dragger {...upload_props} style={{width: 350}}>
-				<p className="ant-upload-drag-icon">
-					<InboxOutlined />
-				</p>
-				<p className="ant-upload-text">Click or drag file to this area to upload</p>
-				<p className="ant-upload-hint">
-					Support for a single or multiple pictures.
-				</p>
-  			</Dragger>
-			<div style={{margin: 15}}>
-				<Popconfirm placement="bottom" 
-					onConfirm={handleCancel} 
-					title="Are you sure to restart? Your photos won't be added."
-					okText="Yes"
-					cancelText="No"
-				>
-					<Button style={{marginRight: 5, width: 75}} disabled={fileList.length === 0}> Reset </Button>
-				</Popconfirm>
-				<Button 
-					type="primary"
-					onClick={handleUpload}
-					disabled={fileList.length === 0}
-					loading={uploading}
-					style={{marginRight: 5, width: 75}}
-				>
-					OK
-				</Button>
-			</div>
-  			</div>
+       		<div style={{width: "50%"}}>
+       			<div className="upload-dragger">
+       				<h3 style={{color: "white"}}> Upload your pictures here! </h3>
+       				<div style={{width: 337, height: 150}}>
+       					<Dragger {...upload_props} >
+       						<div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+										<p className="ant-upload-drag-icon">
+											<InboxOutlined style={{fontSize: 50}}/>
+										</p>
+									</div>
+  							</Dragger>
+  						</div>
+  						{(fileList.length === 0)?<></>:
+							<div style={{margin: 15, position: "absolute", bottom: 0}}>
+								<Popconfirm placement="bottom" onConfirm={handleCancel} 
+								title="Are you sure to restart? Your photos won't be added."
+								okText="Yes"cancelText="No">
+									<Button style={{marginRight: 5, width: 75}}> Reset </Button>
+								</Popconfirm>
+								<Button type="primary" onClick={handleUpload}
+								loading={uploading} style={{marginRight: 5, width: 75}}>
+									OK
+								</Button>
+							</div>
+							}
+  					</div>
+					</div>
+				</div>
 			</div>
 		</>
 	)
