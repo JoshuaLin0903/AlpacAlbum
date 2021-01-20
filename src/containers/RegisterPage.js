c6d7b2f3050a8e5c124801cced234436b63e8065
JoshuaLin0903 <b06901016@ntu.edu.tw>
2021-01-21 01:39:26 +0800

    remove localhost:3000

--- src/containers/RegisterPage.js
+++ src/containers/RegisterPage.js
@@ -53,7 +53,7 @@ const RegisterPage=()=>{
             const {data} = await register({variables: { name: username, password: password, email: email, avatar:default_avatar}})
             console.log(data.registerUser)
             message.success('Successfully registered!')
-            window.location.assign("http://localhost:3000/#/")
+            window.location.assign(window.location.href.replace("register", ""))
         } catch(e){
             console.log(e.message)
             if(e.message.includes("GraphQL error: E11000 duplicate key error")){
